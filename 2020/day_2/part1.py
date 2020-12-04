a12002e64cc47b7fd2d469e4b19907dc570fd241
Nate Emmons <nateemmons@gmail.com>
2020-12-04 02:08:52 -0500

    Clean up 2020 Day 2

--- 2020/day_2/part1.py
+++ 2020/day_2/part1.py
@@ -1,19 +1,53 @@
 import re
+from typing import List
 
-#https://adventofcode.com/2020/day/2
+# https://adventofcode.com/2020/day/2
 
-def check_password_policies(file='input'):
-    valid_password_count = 0
+
+class PasswordPolicy(object):
+    char: str
+    char_min_count: int
+    char_max_count: int
+
+    def __init__(self, char: str, char_min_count: int, char_max_count: int):
+        if len(char) > 1:
+            raise Exception("char should be a single character")
+
+        self.char = char
+        self.char_min_count = char_min_count
+        self.char_max_count = char_max_count
+
+    def check(self, password: str):
+        return self.char_min_count <= password.count(self.char) <= self.char_max_count
+
+def process_input(file='input') -> List[dict]:
+    passwords_and_policies = []
     with open(file) as f:
         for line in f:
             result = re.search("(\d+)-(\d+) ([a-z]): ([a-z]+)", line)
             if result:
                 (char_count_min, char_count_max, char, password) = result.groups()
 
-                if int(char_count_min) <= password.count(char) <= int(char_count_max):
-                    valid_password_count += 1
+                passwords_and_policies.append({
+                    'password': password,
+                    'policy': PasswordPolicy(char, int(char_count_min), int(char_count_max))
+                })
             else:
                 raise Exception("Improper formatting for line: " + line)
+    return passwords_and_policies
+
+
+def check_password_policies():
+    passwords_and_policies = process_input()
+
+    valid_password_count = 0
+
+    for item in passwords_and_policies:
+        policy = item['policy']  # type: PasswordPolicy
+        password = item['password']
+
+        if policy.check(password):
+            valid_password_count += 1
     return valid_password_count
 
 print(check_password_policies())
--- 2020/day_2/part2.py
+++ 2020/day_2/part2.py
@@ -1,22 +1,59 @@
 import re
+from typing import List
 
-#https://adventofcode.com/2020/day/2
+# https://adventofcode.com/2020/day/2
 
-def check_password_policies(file='input'):
-    valid_password_count = 0
+
+class PasswordPolicy(object):
+    char: str
+    char_pos_1: int
+    char_pos_2: int
+
+    def __init__(self, char: str, char_pos_1: int, char_pos_2: int):
+        if len(char) > 1:
+            raise Exception("char should be a single character")
+
+        self.char = char
+        self.char_pos_1 = char_pos_1
+        self.char_pos_2 = char_pos_2
+
+    def check(self, password: str):
+        char_in_pos_1 = (password[self.char_pos_1 - 1] == self.char)
+        char_in_pos_2 = (password[self.char_pos_2 - 1] == self.char)
+
+        # the char should only be filled in one of the two positions
+        return (int(char_in_pos_1) + int(char_in_pos_2)) == 1
+
+
+def process_input(file='input') -> List[dict]:
+    passwords_and_policies = []
     with open(file) as f:
         for line in f:
             result = re.search("(\d+)-(\d+) ([a-z]): ([a-z]+)", line)
             if result:
-                (pos_1, pos_2, char, password) = result.groups()
+                (char_pos_1, char_pos_2, char, password) = result.groups()
 
-                pos_1_correctly_filled = (password[int(pos_1) - 1] == char)
-                pos_2_correctly_filled = (password[int(pos_2) - 1] == char)
-
-                if (int(pos_1_correctly_filled) + int(pos_2_correctly_filled)) == 1:
-                    valid_password_count += 1
+                passwords_and_policies.append({
+                    'password': password,
+                    'policy': PasswordPolicy(char, int(char_pos_1), int(char_pos_2))
+                })
             else:
                 raise Exception("Improper formatting for line: " + line)
+    return passwords_and_policies
+
+
+def check_password_policies():
+    passwords_and_policies = process_input()
+
+    valid_password_count = 0
+
+    for item in passwords_and_policies:
+        policy = item['policy']  # type: PasswordPolicy
+        password = item['password']
+
+        if policy.check(password):
+            valid_password_count += 1
     return valid_password_count
+
 
 print(check_password_policies())
