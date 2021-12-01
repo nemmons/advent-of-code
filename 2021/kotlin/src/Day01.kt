e0d7251f599e008b669ea62e4b08db6df3e79f96
Nate Emmons <nateemmons@gmail.com>
2021-12-01 13:12:10 -0500

    2021 Kotlin Day 1

--- 2021/kotlin/src/Day01.kt
+++ 2021/kotlin/src/Day01.kt
@@ -1,15 +1,23 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }
+            .zipWithNext()
+            .filter { pair -> pair.second > pair.first }
+            .count()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }
+            .windowed(3, 1)
+            .map { it.reduce{x,y -> x+y} }
+            .zipWithNext()
+            .filter { pair -> pair.second > pair.first }
+            .count()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
