6ec8925aa12be86bd80347dcc4b9c8b2dff17967
Nate Emmons <nateemmons@gmail.com>
2021-12-01 00:41:17 -0500

    Rust 2021 Day 1 cleanup

--- 2021/rust/day01/src/main.rs
+++ 2021/rust/day01/src/main.rs
@@ -1,47 +1,46 @@
 use itertools::Itertools;
 
 fn main() {
-    println!("Part 1: {}; Part 2: {}",
-     evaluate_part_1(include_str!("../input.txt")),
-     evaluate_part_2(include_str!("../input.txt"))
+    println!(
+        "Part 1: {}; Part 2: {}",
+        evaluate_part_1(include_str!("../input.txt")),
+        evaluate_part_2(include_str!("../input.txt"))
     )
 }
 
 fn evaluate_part_1(string: &str) -> usize {
-    let depths: Vec<usize> = string
+    let depth_measurements: Vec<usize> = string
         .lines()
         .map(|i| i.parse::<usize>().unwrap())
         .collect();
 
-    let mut deeper_count: usize = 0;
+    let mut increasing_depth_count: usize = 0;
 
-    for (index, depth) in depths.iter().enumerate() {
-        if index > 0 && depth > depths.get(index-1).unwrap() {
-            deeper_count = deeper_count + 1;
+    for i in 1..depth_measurements.len() {
+        let measurement = depth_measurements.get(i).unwrap();
+        let prev_measurement = depth_measurements.get(i - 1).unwrap();
+        if measurement > prev_measurement {
+            increasing_depth_count += 1;
         }
     }
 
-    deeper_count
+    increasing_depth_count
 }
 
 fn evaluate_part_2(string: &str) -> usize {
-    let depths: Vec<usize> = string
+    let depth_measurements: Vec<usize> = string
         .lines()
         .map(|i| i.parse::<usize>().unwrap())
         .collect();
 
     let mut deeper_count: usize = 0;
 
-    for i in 1..(depths.len()-2) {
-        let window = depths.get(i-1).unwrap()
-            + depths.get(i).unwrap()
-            + depths.get(i+1).unwrap();
+    for i in 1..(depth_measurements.len() - 2) {
+        let window_1_start = depth_measurements.get(i - 1).unwrap();
+        // depths[i] and depths[i+1] are shared between both sliding windows
+        let window_2_end = depth_measurements.get(i + 2).unwrap();
 
-        let next_window = depths.get(i).unwrap()
-            + depths.get(i+1).unwrap()
-            + depths.get(i+2).unwrap();
-
-        if next_window > window {
+        if window_2_end > window_1_start {
             deeper_count += 1;
         }
     }
@@ -52,7 +51,7 @@ fn evaluate_part_2(string: &str) -> usize {
 #[cfg(test)]
 mod test {
     use crate::evaluate_part_1;
-   use crate::evaluate_part_2;
+    use crate::evaluate_part_2;
 
     #[test]
     fn it_calculates_the_sample_correctly_for_part_1() {
