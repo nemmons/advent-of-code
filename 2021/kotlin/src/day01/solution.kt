aa759a2d4fd0b9cfa0deac14df830f494306fbd0
Nate Emmons <nateemmons@gmail.com>
2021-12-01 22:12:52 -0500

    More Kotlin 2021 Day 1 cleanup

--- 2021/kotlin/src/day01/solution.kt
+++ 2021/kotlin/src/day01/solution.kt
@@ -1,28 +1,33 @@
 package day01
 
 import utils.readInput
+import utils.toInts
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.map { it.toInt() }
+    fun countIncreases(input: List<Int>): Int {
+        return input
             .zipWithNext()
             .filter { pair -> pair.second > pair.first }
             .count()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.map { it.toInt() }
+    fun part1(input: List<Int>): Int {
+        return countIncreases(input)
+    }
+
+    fun part2(input: List<Int>): Int {
+        return input
             .windowed(3, 1) { it.sum() }
-            .zipWithNext()
-            .filter { pair -> pair.second > pair.first }
-            .count()
+            .let { countIncreases(it) }
     }
 
     val testInput = readInput("day01/test")
-    check(part1(testInput) == 7)
-    check(part2(testInput) == 5)
+    check(part1(testInput.toInts()) == 7)
+    check(part2(testInput.toInts()) == 5)
 
     val input = readInput("day01/input")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input.toInts()))
+    println(part2(input.toInts()))
 }
+
+
--- 2021/kotlin/src/utils/Utils.kt
+++ 2021/kotlin/src/utils/Utils.kt
@@ -13,3 +13,6 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to Day01.md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+
+fun List<String>.toInts() = this.map { it.toInt() }
