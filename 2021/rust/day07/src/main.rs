12305b2ac89e9f142e5ee4cf11f3b0f390f352a9
Nate Emmons <nateemmons@gmail.com>
2021-12-07 01:14:24 -0500

    Rust 2021 Day 7

---
+++ 2021/rust/day07/src/main.rs
@@ -0,0 +1,66 @@
+use std::ops::{Div, Mul};
+
+fn main() {
+    println!(
+        "Part 1: {}; Part 2: {}",
+        evaluate_part_1(include_str!("../input.txt")),
+        evaluate_part_2(include_str!("../input.txt"))
+    )
+}
+
+fn evaluate_part_1(string: &str) -> isize {
+    let crabs: Vec<isize> = string.split(',')
+        .map(|s| s.parse::<isize>().unwrap())
+        .collect();
+
+    crabs.iter()
+        .map(|c| calculate_total_fuel_for_constant_travel_cost(&crabs, c))
+        .min().unwrap()
+}
+
+fn calculate_total_fuel_for_constant_travel_cost(crabs: &Vec<isize>, target_pos: &isize) -> isize {
+    crabs.iter()
+        .map(|c| isize::abs(c - target_pos)).sum::<isize>()
+}
+
+fn evaluate_part_2(string: &str) -> isize {
+    let crabs: Vec<isize> = string.split(',')
+        .map(|s| s.parse::<isize>().unwrap())
+        .collect();
+
+    let max_pos = crabs.iter().max().unwrap();
+    (0..(max_pos+1))
+        .map(|c| calculate_total_fuel_for_increasing_travel_cost(&crabs, c))
+        .min().unwrap()
+}
+
+fn calculate_total_fuel_for_increasing_travel_cost(crabs: &Vec<isize>, target_pos: isize) -> isize {
+    (*crabs).iter()
+        .map(|c| {
+            let distance = isize::abs(c - target_pos);
+            calculate_travel_cost(distance)
+        })
+        .sum::<isize>()
+}
+
+fn calculate_travel_cost(distance: isize) -> isize {
+   distance
+        .mul(distance + 1)
+        .div(2)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::evaluate_part_1;
+    use crate::evaluate_part_2;
+
+    #[test]
+    fn it_calculates_the_sample_correctly_for_part_1() {
+        assert_eq!(37, evaluate_part_1(include_str!("../sample.txt")));
+    }
+
+    #[test]
+    fn it_calculates_the_sample_correctly_for_part_2() {
+        assert_eq!(168, evaluate_part_2(include_str!("../sample.txt")));
+    }
+}
