6f0c912c088ee51c872a92b325a74a7ae2671d5a
Nate Emmons <nateemmons@gmail.com>
2021-12-21 22:33:38 -0500

    2021 Day 15 Kotlin part 2
    - Hacky implementation of Dijkstra's algorithm

--- 2021/kotlin/src/day15/solution.kt
+++ 2021/kotlin/src/day15/solution.kt
@@ -4,31 +4,35 @@ import utils.readInput
 import utils.toInts
 import java.util.*
 
-
+data class Point(val x: Int, val y: Int)
+data class Node(
+    val pos: Point,
+    val travelCost: Int,
+    var tentativeDistance: Int,
+    var visited: Boolean = false,
+) : Comparable<Node> {
+    override fun compareTo(other: Node): Int {
+        return this.tentativeDistance - other.tentativeDistance
+    }
+}
 
 fun main() {
 
-    fun calcRisk(grid: List<List<Int>>, runningRisk: Int, x: Int, y: Int): List<Int> {
-        val newRisk = if (y > 0 || x > 0) {
-            grid[y][x]
-        } else { 0 }
-
-        if (y == grid.size - 1 && x == grid[0].size - 1) {
-            return listOf(runningRisk + newRisk)
+    fun getNeighbors(nodes: List<List<Node>>, point: Point): List<Node> {
+        val neighbors = mutableListOf<Node>()
+//        if (point.y > 0) {
+//            neighbors.add(nodes[point.y-1][point.x])
+//        }
+        if (point.y < nodes.size - 1) {
+            neighbors.add(nodes[point.y + 1][point.x])
         }
-
-        val dests = mutableListOf<Pair<Int,Int>>()
-
-        if (y < grid.size - 1) {
-            dests.add(Pair(x,y+1))
+        if (point.x < nodes[0].size - 1) {
+            neighbors.add(nodes[point.y][point.x + 1])
         }
-        if (x < grid[0].size - 1) {
-            dests.add(Pair(x+1,y))
-        }
-
-        return dests.flatMap {
-            calcRisk(grid, runningRisk + newRisk, it.first, it.second)
-        }
+//        if (point.x < 0) {
+//            neighbors.add(nodes[point.y][point.x-1])
+//        }
+        return neighbors
     }
 
     fun part1(lines: List<String>): Int {
@@ -36,21 +40,133 @@ fun main() {
             it.split("").filter { char -> char != "" }.toInts()
         }
 
-        return calcRisk(grid, 0, 0, 0).minOrNull()!!
+        val nodes: List<List<Node>> = grid.indices.map { y ->
+            grid[y].indices.map { x ->
+                Node(
+                    Point(x, y),
+                    if (x == 0 && y == 0) 0 else grid[y][x],
+                    if (x == 0 && y == 0) 0 else 9999,
+                )
+            }
+        }
+
+        val maxY = grid.size - 1
+        val maxX = grid[0].size - 1
+
+        var searchNode: Node = nodes[0][0]
+
+        while (!nodes[maxY][maxX].visited) {
+            getNeighbors(nodes, searchNode.pos)
+                .filter { !it.visited }
+                .forEach {
+                    it.tentativeDistance = minOf(it.tentativeDistance, it.travelCost + searchNode.tentativeDistance)
+                }
+
+            searchNode.visited = true
+
+            val remainingNodes = nodes.flatten()
+                .filter { !it.visited }
+                .sortedBy { it.tentativeDistance }
+            if (remainingNodes.isNotEmpty()) {
+                searchNode = remainingNodes.first()
+            }
+        }
+
+        return nodes[maxY][maxX].tentativeDistance
     }
 
 
     fun part2(lines: List<String>): Int {
-        return 2
+        val grid: List<List<Int>> = lines.map {
+            it.split("").filter { char -> char != "" }.toInts()
+        }
+
+        val sectionYSize = grid.size
+        val sectionXSize = grid[0].size
+
+        val fullgrid = (0 until sectionYSize * 5).map { y ->
+            (0 until sectionXSize * 5).map { x ->
+                val sectionX = x.div(sectionXSize)
+                val sectionY = y.div(sectionYSize)
+                val sectionXVal = x.mod(sectionXSize)
+                val sectionYVal = y.mod(sectionYSize)
+
+                val result = (grid[sectionYVal][sectionXVal] + sectionX + sectionY)
+                if (result > 9) result - 9 else result
+            }
+
+        }
+
+        val maxY = fullgrid.size - 1
+        val maxX = fullgrid[0].size - 1
+
+        val nodes: List<List<Node>> = fullgrid.indices.map { y ->
+            fullgrid[y].indices.map { x ->
+                Node(
+                    Point(x, y),
+                    if (x == 0 && y == 0) 0 else fullgrid[y][x],
+                    if (x == 0 && y == 0) 0 else 9999,
+                )
+            }
+        }
+
+        val unvisited = PriorityQueue(nodes.flatten())
+
+        var searchNode: Node = nodes[0][0]
+
+        while (!nodes[maxY][maxX].visited) {
+            if (searchNode.pos.x > 0) {
+                nodes[searchNode.pos.y][searchNode.pos.x - 1].let {
+                    if (!it.visited) {
+                        unvisited.remove(it)
+                        it.tentativeDistance = minOf(it.tentativeDistance, it.travelCost + searchNode.tentativeDistance)
+                        unvisited.add(it)
+                    }
+                }
+            }
+            if (searchNode.pos.y > 0) {
+                nodes[searchNode.pos.y - 1][searchNode.pos.x].let {
+                    if (!it.visited) {
+                        unvisited.remove(it)
+                        it.tentativeDistance = minOf(it.tentativeDistance, it.travelCost + searchNode.tentativeDistance)
+                        unvisited.add(it)
+                    }
+                }
+            }
+            if (searchNode.pos.x < maxX) {
+                nodes[searchNode.pos.y][searchNode.pos.x + 1].let {
+                    if (!it.visited) {
+                        unvisited.remove(it)
+                        it.tentativeDistance = minOf(it.tentativeDistance, it.travelCost + searchNode.tentativeDistance)
+                        unvisited.add(it)
+                    }
+                }
+            }
+            if (searchNode.pos.y < maxY) {
+                nodes[searchNode.pos.y + 1][searchNode.pos.x].let {
+                    if (!it.visited) {
+                        unvisited.remove(it)
+                        it.tentativeDistance = minOf(it.tentativeDistance, it.travelCost + searchNode.tentativeDistance)
+                        unvisited.add(it)
+                    }
+                }
+            }
+            searchNode.visited = true
+
+            if (unvisited.size > 0) {
+                searchNode = unvisited.poll()
+            }
+        }
+
+        return nodes[maxY][maxX].tentativeDistance
     }
 
     val testInput = readInput("day15/test")
 
     check(part1(testInput) == 40)
-    check(part2(testInput) == 2)
+    check(part2(testInput) == 315)
 
     val input = readInput("day15/input")
     println(part1(input))
     println(part2(input))
 }
-
