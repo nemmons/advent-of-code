92543b9e9636cc4f6cca00f0cfeb19a52d568840
Nate Emmons <nateemmons@gmail.com>
2023-12-01 15:59:18 -0500

    refactor day01 from regex to 'parsing'

--- 2023/kotlin/src/Day01.kt
+++ 2023/kotlin/src/Day01.kt
@@ -1,25 +1,26 @@
+private val validTokensPart1 = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9")
+private val validTokensPart2 = listOf(
+    "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+)
+
+
 fun main() {
     fun part1(input: List<String>): Int {
-        val regex = "\\d".toRegex()
-
-        return input.map { line ->
-            regex.findAll(line)
-                .map { it.value }
-                .let { "${it.first()}${it.last()}".toInt() }
+        return input.map {
+            it.parseTokens(validTokensPart1)
+                .let { tokens ->
+                    tokens.first().toInt() * 10 + tokens.last().toInt()
+                }
         }.reduce { acc, num -> acc + num }
-
     }
 
     fun part2(input: List<String>): Int {
-        //use lookahead to prevent matches from being consumed (i.e. otherwise, in 'oneight' consuming the 'one' prevents us from finding the 'eight')
-        val regex = "(?=(\\d|one|two|three|four|five|six|seven|eight|nine))".toRegex()
-
-        return input.map { line ->
-            regex.findAll(line)
-                .map {
-                    it.groups[1]!!.value
+        return input.map {
+            it.parseTokensBetter(validTokensPart2)
+                .let { tokens ->
+                    tokens.first().numVal() * 10 + tokens.last().numVal()
                 }
-                .let { "${it.first().numVal()}${it.last().numVal()}".toInt() }
         }.reduce { acc, num -> acc + num }
     }
 
@@ -34,7 +35,36 @@ fun main() {
     part2(input).println()
 }
 
-fun String.numVal(): Int {
+private fun String.parseTokens(validTokens: List<String>): List<String> {
+    val tokens = mutableListOf<String>()
+    for (i in this.indices) {
+        val peek = this.substring(i, i + 1)
+        if (validTokens.contains(peek)) {
+            tokens.add(peek)
+        }
+    }
+    return tokens
+}
+
+private fun String.parseTokensBetter(validTokens: List<String>): List<String> {
+    val tokens = mutableListOf<String>()
+
+    var i = 0
+    while (i < this.length) {
+        for (j in listOf(0,2,3,4)) { // the 2..4 range is because i know all the string tokens are 3-5 chars long
+            if (validTokens.contains(this.substring(i, minOf(i + j + 1, this.length)))) {
+                tokens.add(this.substring(i, i + j + 1))
+                i += maxOf(j - 1, 0) //skip ahead a bit but allow one char for overlap for 'oneight', 'twone', etc
+                break
+            }
+        }
+        i++
+    }
+
+    return tokens
+}
+
+private fun String.numVal(): Int {
     return when (this) {
         "one" -> 1
         "two" -> 2
