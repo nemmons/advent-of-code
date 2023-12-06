7472eebd48e172b8af1ea1a634cfea80b99f73c4
Nate Emmons <nateemmons@gmail.com>
2023-12-06 00:00:29 -0500

    Day 5 part 2

--- 2023/kotlin/src/Day05.kt
+++ 2023/kotlin/src/Day05.kt
@@ -1,10 +1,9 @@
 fun main() {
+    val labelRegex = "([a-z]+)-to-([a-z]+) map:".toRegex()
 
     fun part1(input: List<String>): Long {
         val (_, seedStr) = input[0].split(':')
         val seeds = seedStr.trim().split(' ').map { it.toLong() }
-
-        val labelRegex = "([a-z]+)-to-([a-z]+) map:".toRegex()
 
         var (fromLbl, toLbl) = "" to ""
 
@@ -48,11 +47,9 @@ fun main() {
         val (_, seedStr) = input[0].split(':')
         val seedPairs = seedStr.trim().split(' ').map { it.toLong() }.chunked(2)
 
-        val labelRegex = "([a-z]+)-to-([a-z]+) map:".toRegex()
-
         var (fromLbl, toLbl) = "" to ""
 
-        val maps = mutableMapOf<Pair<String, String>, MutableMap<Pair<Long, Long>, Pair<Long, Long>>>()
+        val maps = mutableMapOf<Pair<String, String>, MutableMap<Range, Range>>()
 
         input.subList(2, input.size - 1).forEach { line ->
             if (labelRegex.matches(line)) {
@@ -63,44 +60,45 @@ fun main() {
             } else if (line.trim().isNotEmpty()) {
                 val (toStart, fromStart, rangeLength) = line.trim().split(' ').map { it.toLong() }
 
-                maps[fromLbl to toLbl]!![fromStart to fromStart + rangeLength - 1] = toStart to toStart + rangeLength - 1
+                maps[fromLbl to toLbl]!![Range.build(fromStart, rangeLength)] = Range.build(toStart, rangeLength)
             }
         }
 
-        return seedPairs.minOf { (seedStart, seedCount) ->
-            println(seedStart to seedCount)
-            var from = "seed"
-            var to: String
+        //initialize starting conditions for loop
+        var inputRanges = seedPairs.map { Range.build(it[0], it[1]) }
+        var outputRanges: MutableList<Range>
+        fromLbl = "seed"
+        toLbl = ""
 
-            var nextMapInputs = mutableListOf<LongRange>(
-                (seedStart until seedStart + seedCount)
-            )
-            do {
-                val mapInputs = nextMapInputs
-                nextMapInputs = mutableListOf()
-                val mapKey = maps.keys.first { it.first == from }
-                to = mapKey.second
+        do {
+            outputRanges = mutableListOf()
+            val mapKey = maps.keys.first { it.first == fromLbl }
+            toLbl = mapKey.second
+            val unmappedInputs = inputRanges.associateWith { listOf(it) }.toMutableMap()
 
-                val mapsFrom = maps[mapKey]!!
+            //  for each translation map range, find intersection(s) with X ranges
+            //      for each intersection, do the mapping and subtract the intersection from 'unmapped ranges'
+            //  move all leftover unmapped ranges into the mapped ranges (this part is messy - I'm keeping a separate copy
+            //      of the input ranges and subtracting out the matches, since modifying the input ranges while iterating
+            //      over them could prove difficult. or I'm lazy.)
 
-                mapInputs.forEach { inputRange ->
-                    mapsFrom.forEach { (from, to) ->
-                        val (fromStart, fromStop) = from
-                        val overlap = inputRange.intersect(fromStart..fromStop)
-                        if (overlap.isNotEmpty()) {
-                            val firstTo = to.first + (overlap.first() - inputRange.first)
-                            nextMapInputs.add(firstTo until firstTo+overlap.size)
-                        }
-
+            maps[mapKey]!!.entries.forEach { (fromRange, toRange) ->
+                inputRanges.forEach { inputRange ->
+                    inputRange.intersect(fromRange)?.let{ match ->
+                        val outputStart = toRange.start + (match.start - fromRange.start)
+                        outputRanges.add(Range.build(outputStart, match.len))
+                        unmappedInputs[inputRange] = unmappedInputs[inputRange]!!.flatMap { it.subtract(match) }
                     }
                 }
+            }
 
-                mapInputs.
+            unmappedInputs.values.flatten().forEach { outputRanges.add(it) }
 
-                from = to
-            } while (to != "location")
-            nextMapInputs.minOf { it.first }
-        }
+            fromLbl = toLbl
+            inputRanges = outputRanges
+        } while (toLbl != "location")
+
+        return inputRanges.minOf { it.start }
     }
 
     val testInput = readTestInput("Day05")
@@ -108,7 +106,44 @@ fun main() {
     check(part2(testInput) == 46L)
 
     val input = readInput("Day05")
-    //part1(input).println()
+    part1(input).println()
     part2(input).println()
+}
+
+data class Range(val start: Long, val end: Long) {
+    val len = end - start + 1
+    companion object {
+        fun build(start: Long, len: Long): Range {
+            return Range(start, start + len - 1)
+        }
+    }
+
+    fun intersect(other: Range): Range? {
+        val newStart = maxOf(start, other.start)
+        val newEnd = minOf(end, other.end)
+
+        return if (newStart <= newEnd) {
+            Range(newStart, newEnd)
+        } else {
+            null
+        }
+    }
+
+    fun subtract(other: Range): List<Range> {
+        if (start > other.end || end < other.start) {
+            return listOf(this)
+        }
+
+        val results = mutableListOf<Range>()
+
+        if (start < other.start) {
+            results.add(Range(start, other.start - 1))
+        }
+
+        if (end > other.end) {
+            results.add(Range(other.end + 1, end))
+        }
+        return results
+    }
 }
 
